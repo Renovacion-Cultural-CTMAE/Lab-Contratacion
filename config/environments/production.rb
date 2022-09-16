54140bd871fdb619a43c7f2de5caace93debb553
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2022-09-16 15:53:31 -0500

    cambios para deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
  # config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+  config.action_cable.allowed_request_origins = [/https:\/\/*/]
+
 
 
   # Compress CSS using a preprocessor.
