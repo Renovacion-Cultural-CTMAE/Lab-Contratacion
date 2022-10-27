74d24de4e12cf00671b38047ccd79b0692a22e9c
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2022-10-27 09:50:43 -0500

    Se añadio la funcionalidad de que si se borra un estudio previo se borran los que estan asociados a el

--- app/models/previous_study.rb
+++ app/models/previous_study.rb
@@ -2,11 +2,11 @@ class PreviousStudy < ApplicationRecord
   has_rich_text :justificacion_necesidad_contratacion
   has_rich_text :obligaciones
   has_many :programs
-  has_one :memorandum_contracting
-  has_one :contracting_authorization
-  has_one :equal_object
-  has_one :personal_inexistence
-  has_one :inclusion_object_acqu
-  has_one :anexo_plan_contratacion
-  has_one :riesgo
+  has_one :memorandum_contracting, dependent: :destroy
+  has_one :contracting_authorization, dependent: :destroy
+  has_one :equal_object, dependent: :destroy
+  has_one :personal_inexistence, dependent: :destroy
+  has_one :inclusion_object_acqu, dependent: :destroy
+  has_one :anexo_plan_contratacion, dependent: :destroy
+  has_one :riesgo, dependent: :destroy
 end
